533db275368cb3b7f735c16b454bf7e1de2dc4d9
Vitor Lupinetti <vitorlupinetti@MacBook-Air-de-Vitor.local>
2020-06-28 23:42:40 -0300

    challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,18 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -15,14 +24,36 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const arr = this.transactions;
+    let income = 0;
+    let outcome = 0;
+    arr.forEach(element => {
+      if (element.type === 'income') {
+        income += element.value;
+      } else {
+        outcome += element.value;
+      }
+    });
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,27 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import Transaction from '../models/Transaction';
 
 const transactionRouter = Router();
+const transactionsRepository = new TransactionsRepository();
 
 // const transactionsRepository = new TransactionsRepository();
 
+// eslint-disable-next-line consistent-return
 transactionRouter.get('/', (request, response) => {
   try {
     // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    const transactionsReturn = {
+      transactions,
+      balance,
+    };
+
+    return response.json(transactionsReturn);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -18,6 +30,11 @@ transactionRouter.get('/', (request, response) => {
 transactionRouter.post('/', (request, response) => {
   try {
     // TODO
+    const { title, value, type } = request.body;
+    const service = new CreateTransactionService(transactionsRepository);
+    const transaction = service.execute({ title, value, type });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
